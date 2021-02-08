3aab5deb986315121d893baf51bd3ccd4529d25e
Hakan Han <eenestatli@gmail.com>
2021-02-08 13:04:50 +0300

    Patch: Login route created

--- api/User/index.js
+++ api/User/index.js
@@ -9,9 +9,29 @@ class User {
 
   authRoutes() {
     this.router.post('/auth/register/', this.registerUser.bind(this));
+    this.router.post('/auth/register/', this.loginUser.bind(this));
   }
 
   async loginUser(req, res) {
+    if (!req || !req.body || !req.body.email || !req.body.password) {
+      res.sendStatus(422); // unprocessable entity
+      return;
+    }
+
+    const user = await UserModel.login(req.body.email, req.body.password);
+    try {
+      if (!user || !user.email) {
+        res.sendStatus(422);
+        return;
+      }
+      res.json(user);
+    } catch (error) {
+      logger.error(error);
+      res.sendStatus(500);
+    }
+  }
+
+  async registerUser(req, res) {
     if (
       !req ||
       !req.body ||
