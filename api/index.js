71046f22c845f6803e341b7bd6da67dc15a4e6a3
Hakan Han <eenestatli@gmail.com>
2021-02-08 13:24:26 +0300

    Patch: Login & Register route connected and tested

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  res.send('hello from test api route');
-});
+const { User } = require('./User');
+
+new User(router);
 
 module.exports = router;
