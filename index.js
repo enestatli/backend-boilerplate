5491f88b1215fb570a17510006b569149cbda84f
Hakan Han <eenestatli@gmail.com>
2021-02-08 12:04:33 +0300

    New: Middleware base created

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ require('dotenv').config();
 
 const { config } = require('./config');
 const { logger } = require('./logger');
+const { useMiddlewares } = require('./middleware');
 
 const app = express();
+
+useMiddlewares(app);
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.listen(config.port, () => logger.info(`Express is on ${config.port}`));
+app.listen(config.port, () =>
+  logger.info(`Express is on http://localhost:${config.port}`)
+);
