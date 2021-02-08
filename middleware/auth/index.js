51f22e6bc0d0825346e91942a916e42ef5a00db0
Hakan Han <eenestatli@gmail.com>
2021-02-08 15:55:59 +0300

    Patch: Auth middlewares created / cookie parser middleware

---
+++ middleware/auth/index.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+
+const UserModel = require('../../db/models/user');
+const { logger } = require('../../logger');
+
+// TODO: put middlewares into class
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, 'my little secret', (err, decodedToken) => {
+      if (err) {
+        logger.error(err.message);
+        // TODO check status code
+        //TODO mb user redirect to the login
+        return;
+      }
+      return next();
+    });
+  }
+};
+
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    res.locals.user = null;
+    return next();
+  }
+  jwt.verify(token, 'my little secret', async (err, decodedToken) => {
+    if (err) {
+      logger.error(err.message);
+      res.locals.user = null;
+      return next();
+    }
+    let user = await UserModel.findById(decodedToken.id);
+    res.local.user = user;
+    return next();
+  });
+};
+
+module.exports = { requireAuth, checkUser };
--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
 const compression = require('compression');
+const cookieParser = require('cookie-parser');
 
 const useMiddlewares = (app) => {
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,6 +10,7 @@ const useMiddlewares = (app) => {
   app.use(helmet());
   app.use(compression());
   app.use(cors());
+  app.use(cookieParser());
 };
 
 module.exports = { useMiddlewares };
