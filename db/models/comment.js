28029cfb6de2dc753d75b59b9f55e7979bc1e505
Hakan Han <eenestatli@gmail.com>
2021-02-13 21:45:01 +0300

    New: CommentModel created

---
+++ db/models/comment.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    postId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Entity',
+    },
+    content: {
+      type: String,
+    },
+  },
+  { timestamps: { createdAt: 'created_at', updatedAt: false } }
+);
+
+var Comment = mongoose.model('comment', CommentSchema);
+
+module.exports = Comment;
+
+//TODO: if needed add later!
+
+// responseTo: {
+//   type: Schema.Types.ObjectId,
+//   ref: 'User',
+// },
