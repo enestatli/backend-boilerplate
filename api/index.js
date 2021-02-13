e90a6241715f01ac88bb762b1a633ee46af29dd2
Hakan Han <eenestatli@gmail.com>
2021-02-13 21:45:43 +0300

    Ignore: saveComment route tested

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const { Auth } = require('./Auth');
+const { Comment } = require('./Comment');
 
 new Auth(router);
+new Comment(router);
 
 module.exports = router;
