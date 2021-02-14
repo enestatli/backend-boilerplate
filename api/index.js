636045fb8a90ecf435b853d0896ebec6b0492ecd
Hakan Han <eenestatli@gmail.com>
2021-02-14 20:09:08 +0300

    Ignore: Like routes tested

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const { Auth } = require('./Auth');
 const { Comment } = require('./Comment');
 const { Entity } = require('./Entity');
+const { Like } = require('./Like');
 
 new Auth(router);
 new Comment(router);
 new Entity(router);
+new Like(router);
 
 module.exports = router;
