51068f86f15e6f059a58ada73926bede2821058d
Hakan Han <eenestatli@gmail.com>
2021-02-15 20:59:09 +0300

    Ignore: Tested Dislike routes / renaming

--- api/index.js
+++ api/index.js
@@ -7,11 +7,13 @@ const { Comment } = require('./Comment');
 const { Entity } = require('./Entity');
 const { Like } = require('./Like');
 const { User } = require('./User');
+const { Dislike } = require('./Dislike');
 
 new Auth(router);
 new Comment(router);
 new Entity(router);
 new Like(router);
+new Dislike(router);
 
 new User(router);
 
