8f984ebe536de06da50c3b2d0acf705ffa67f5da
Hakan Han <eenestatli@gmail.com>
2021-02-15 18:40:31 +0300

    Ignore: Tested follow/unfollow route

--- api/index.js
+++ api/index.js
@@ -6,10 +6,13 @@ const { Auth } = require('./Auth');
 const { Comment } = require('./Comment');
 const { Entity } = require('./Entity');
 const { Like } = require('./Like');
+const { User } = require('./User');
 
 new Auth(router);
 new Comment(router);
 new Entity(router);
 new Like(router);
+
+new User(router);
 
 module.exports = router;
